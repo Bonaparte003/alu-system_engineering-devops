91605cfb47f0a79c4f9206c6dfb04316a7fe2708
Bonaparte <b.uteramaho@alustudent.com>
2024-11-25 07:39:51 +0200

    updates

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -3,4 +3,4 @@
 file {'copy_file':
   ensure => file,
   source => '/var/www/html/wp-includes/class-wp-locale.php',
-  path   => '/var/www/html/wp-includes/class-wp-locale.php',}
+  path   => '/var/www/html/wp-includes/class-wp-locale.phpp',}
